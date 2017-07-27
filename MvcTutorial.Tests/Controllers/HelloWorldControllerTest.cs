a26e5fd253f9ba1833184f150aa7db51bf292b98
Shuhei Yamashita <b.e7472@gmail.com>
2017-07-27 18:35:07 +0900

    initial commit

---
+++ MvcTutorial.Tests/Controllers/HelloWorldControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcTutorial;
+using MvcTutorial.Controllers;
+
+namespace MvcTutorial.Tests.Controllers
+{
+    [TestClass]
+    public class HomeWorldControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HelloWorldController controller = new HelloWorldController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Index", result.ViewBag.Message);
+        }
+    }
+}
---
+++ MvcTutorial/Models/MovieDBContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+
+namespace MvcTutorial.Models
+{
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
